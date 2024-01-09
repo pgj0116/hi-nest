2a7e104de8155d626654827b256ca7b3510c4d65
pgj0116 <83294916+pgj0116@users.noreply.github.com>
2024-01-09 15:41:23 +0900

    basic rest api done

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,31 +6,39 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
   @Get()
   getAll() {
-    return 'this will return all movies';
+    return this.moviesService.getAll();
+  }
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after:${searchingYear}`;
   }
 
-  @Get('/:id')
-  getOne(@Param('id') movieId: string) {
-    return 'This will ruturn one movie' + movieId;
+  @Get(':id')
+  getOne(@Param('id') movieId: number) {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
-    console.log(movieData);
-    return 'This will create a moive';
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
   }
-  @Delete(':/id')
-  remove(@Param('id') movieId: string) {
-    return 'This will delete a movie' + movieId;
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
+    return this.moviesService.deleteOne(movieId);
   }
-  @Patch(':/id')
-  patch(@Param('id') movieId: string) {
-    return 'This will patch a movie' + movieId;
+  @Patch(':id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateData);
   }
 }
